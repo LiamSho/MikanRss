0470dd989a7be8a98699c3a1ce48418174dc2c81
Liam Sho <conan_creeper@outlook.com>
2022-07-13 05:34:48 +0800

    fix: fix deserialization issue

--- MikanRss/MikanRssSerializer.cs
+++ MikanRss/MikanRssSerializer.cs
@@ -3,7 +3,7 @@
 // It is not related to Mikan Project.
 // Licensed under the AGPL-3.0 license.
 
-using System.Text;
+using System.Xml;
 using System.Xml.Serialization;
 
 namespace MikanRss;
@@ -18,24 +18,14 @@ public static class MikanRssSerializer
     /// </summary>
     /// <param name="rss">RSS XML 字符串</param>
     /// <returns><see cref="MikanRss"/> 对象或 NULL</returns>
+    /// <exception cref="InvalidOperationException">RSS XML 字符串无法解析</exception>
     public static Model.MikanRss? Deserialize(string rss)
     {
-        var buff = Encoding.UTF8.GetBytes(rss);
-        var stream = new MemoryStream(buff);
-        var obj = Deserialize(stream);
-        stream.Close();
-        return obj;
-    }
-
-    /// <summary>
-    ///     从 <see cref="Stream"/> 反序列化 <see cref="Model.MikanRss"/> 对象
-    /// </summary>
-    /// <param name="rssStream">RSS XML <see cref="Stream"/></param>
-    /// <returns><see cref="MikanRss"/> 对象或 NULL</returns>
-    public static Model.MikanRss? Deserialize(Stream rssStream)
-    {
-        var xml = new XmlSerializer(typeof(Model.MikanRss));
-        var obj = xml.Deserialize(rssStream);
+        using var textReader = new StringReader(rss);
+        using var reader = new XmlTextReader(textReader);
+        reader.Namespaces = false;
+        var serializer = new XmlSerializer(typeof(Model.MikanRss));
+        var obj = serializer.Deserialize(reader);
 
         return obj as Model.MikanRss;
     }
